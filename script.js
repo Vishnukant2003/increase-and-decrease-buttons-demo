0d3f93269c65fbb146da4a45e9bd740f5ae06be5
Vishnukant2003 <vishnukantgadekar2003@gmail.com>
2025-07-14 23:28:25 +0530

    changes

---
+++ script.js
@@ -0,0 +1,11 @@
+const para = document.getElementById("para");
+let count = 0;
+
+function decreaMent(){
+    count--;
+    para.innerHTML =count;
+}
+function increMent(){
+    count++;
+    para.innerHTML =count
+}
