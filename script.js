988ba93aefa487596af1c797252957b72bce8caa
Vishnukant2003 <vishnukantgadekar2003@gmail.com>
2025-07-14 23:41:11 +0530

    adding a reset button

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const para = document.getElementById("para");
 let count = 0;
 
 function decreaMent(){
+    if(count==0){
+        return;
+    }
     count--;
     para.innerHTML =count;
 }
 function increMent(){
     count++;
     para.innerHTML =count
+}
+function reSet(){
+    para.innerHTML = "0";
 }
